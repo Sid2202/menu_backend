6643850ca88303b96c36b102019aa9f1ca157a37
Sidhanti Patil <sidpatil2209@gmail.com>
2022-08-09 00:48:04 +0530

    heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "https://data-management-frontend.vercel.app/"
 };
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
