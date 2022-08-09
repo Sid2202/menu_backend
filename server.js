6fc9214fe1aed3967dfe10fdca1b3931368d0969
Sidhanti <sidpatil2209@gmail.com>
2022-08-09 21:09:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 var corsOptions = {
-  origin: "https://data-management-frontend.vercel.app/"
+  origin: "https://data-management-frontend.vercel.app"
 };
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
